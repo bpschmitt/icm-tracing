0514653bd6095b7d2250571f0810f5b2fec6ea66
uniquejava <uniquejava@gmail.com>
2026-03-09 16:28:59 +0800

    init

---
+++ src/main/java/click/yinsb/icmtracing/IcmTracingApplication.java
@@ -0,0 +1,13 @@
+package click.yinsb.icmtracing;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IcmTracingApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(IcmTracingApplication.class, args);
+    }
+
+}
---
+++ src/main/java/click/yinsb/icmtracing/config/OtelConfig.java
@@ -0,0 +1,45 @@
+package click.yinsb.icmtracing.config;
+
+import io.opentelemetry.api.OpenTelemetry;
+import io.opentelemetry.api.baggage.propagation.W3CBaggagePropagator;
+import io.opentelemetry.api.common.AttributeKey;
+import io.opentelemetry.api.common.Attributes;
+import io.opentelemetry.api.trace.propagation.W3CTraceContextPropagator;
+import io.opentelemetry.context.propagation.ContextPropagators;
+import io.opentelemetry.context.propagation.TextMapPropagator;
+import io.opentelemetry.exporter.otlp.trace.OtlpGrpcSpanExporter;
+import io.opentelemetry.sdk.OpenTelemetrySdk;
+import io.opentelemetry.sdk.resources.Resource;
+import io.opentelemetry.sdk.trace.SdkTracerProvider;
+import io.opentelemetry.sdk.trace.export.BatchSpanProcessor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class OtelConfig {
+    @Bean
+    OpenTelemetry openTelemetry() {
+        Resource resource = Resource.getDefault()
+                .merge(Resource.create(Attributes.of(AttributeKey.stringKey("service.name"), "icm-lite")));
+
+        SdkTracerProvider tracerProvider = SdkTracerProvider.builder()
+                .setResource(resource)
+                .addSpanProcessor(BatchSpanProcessor.builder(
+                                OtlpGrpcSpanExporter.builder()
+                                        .setEndpoint("http://localhost:4317")
+                                        .build())
+                        .build())
+                .build();
+
+
+        return OpenTelemetrySdk.builder()
+                .setTracerProvider(tracerProvider)
+                .setPropagators(ContextPropagators.create(
+                        TextMapPropagator.composite(
+                                W3CTraceContextPropagator.getInstance(),
+                                W3CBaggagePropagator.getInstance()
+                        )))
+                .build();
+    }
+
+}
